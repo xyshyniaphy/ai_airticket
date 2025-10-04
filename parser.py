886565c0bb996a59df2b885cd7eb275e9ab860c9
lmr <xyshyniaphy@gmail.com>
2025-10-04 10:47:23 +0000

    parse is ok

--- parser.py
+++ parser.py
@@ -9,8 +9,11 @@ def clean_html(html: str) -> str:
     for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
         comment.extract()
 
+    for tag in soup.find_all(True):
+        for attr in list(tag.attrs):
+            if attr != 'id' and attr != 'class':
+                del tag[attr]
     return str(soup)
-
 
 def parse_flight_data(soup: BeautifulSoup) -> list[dict]:
     """Parses the flight data from the BeautifulSoup object."""
@@ -19,40 +22,39 @@ def parse_flight_data(soup: BeautifulSoup) -> list[dict]:
 
     for flight in flight_areas:
         provider_name_tag = flight.find('div', class_='flight-summary-hdg')
-        provider_name = provider_name_tag.text.strip() if provider_name_tag else 'N/A'
+        provider_name = provider_name_tag.text.strip().split('\n')[0] if provider_name_tag else 'N/A'
 
-        price_tag = flight.find('span', class_='price')
+        price_tag = flight.find('span', class_='flight-summary-total-price')
         price = price_tag.text.strip() if price_tag else 'N/A'
 
-        sch_header_tag = flight.find('div', class_='sch-header')
-        trip_type = 'N/A'
-        if sch_header_tag:
-            span_tag = sch_header_tag.find('span')
-            if span_tag:
-                trip_type = span_tag.text.strip()
+        trip_type_tag = flight.find('div', class_='sch-header-sup')
+        trip_type = trip_type_tag.text.strip() if trip_type_tag else 'N/A'
 
-        going_area = flight.find('div', class_='going-area')
         dpt_time, dpt_airport, arr_time, arr_airport = 'N/A', 'N/A', 'N/A', 'N/A'
+        going_area = flight.find('div', class_='going-area')
         if going_area:
-            dpt_time_tag = going_area.find('div', class_='dpt-time')
+            dpt_time_tag = going_area.find('span', class_='sch-time')
             dpt_time = dpt_time_tag.text.strip() if dpt_time_tag else 'N/A'
 
-            dpt_airport_tag = going_area.find('div', class_='dpt-airport')
+            dpt_airport_tag = going_area.find('span', class_='city-airport')
             dpt_airport = dpt_airport_tag.text.strip() if dpt_airport_tag else 'N/A'
 
-            arr_time_tag = going_area.find('div', class_='arr-time')
+        return_area = flight.find('div', class_='return-area')
+        if return_area:
+            arr_time_tag = return_area.find('span', class_='sch-time')
             arr_time = arr_time_tag.text.strip() if arr_time_tag else 'N/A'
 
-            arr_airport_tag = going_area.find('div', class_='arr-airport')
+            arr_airport_tag = return_area.find('span', class_='city-airport2')
             arr_airport = arr_airport_tag.text.strip() if arr_airport_tag else 'N/A'
 
-        flt_term_tag = flight.find('div', class_='flt-term')
         duration, transfers = 'N/A', 'N/A'
-        if flt_term_tag:
-            hour_tag = flt_term_tag.find('span', class_='hour')
-            duration = hour_tag.text.strip() if hour_tag else 'N/A'
+        flt_term = flight.find('div', class_='flt-term')
+        if flt_term:
+            duration_tag = flt_term.find('div', class_='flt-term-top')
+            if duration_tag and duration_tag.div:
+                duration = duration_tag.div.text.strip().split('\n')[0]
 
-            transfer_tag = flt_term_tag.find('span', class_='transfer')
+            transfer_tag = flt_term.find('span', class_='flt-term-transit')
             transfers = transfer_tag.text.strip() if transfer_tag else 'N/A'
 
         flight_data = {
